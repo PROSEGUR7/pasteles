eb92372017c0d938cae5b8168461fb8e2b82f9d3
brandon <brandondavidbello@gmail.com>
2026-02-17 17:27:47 -0500

    smjro

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
     distDir: isProduction ? ".next-prod" : ".next-live",
+    serverExternalPackages: ["ffmpeg-static"],
 };
 
 export default nextConfig;
